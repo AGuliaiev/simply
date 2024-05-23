a4a16a6a25fcee487b9c5e3cbedd1c42d979f1c7
Gulini32 <gulini32@gmail.com>
2024-05-23 10:59:49 +0300

    hw

--- src/main/java/com/example/simply/controller/MainController.java
+++ src/main/java/com/example/simply/controller/MainController.java
@@ -1,29 +1,30 @@
 package com.example.simply.controller;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("title", "Главная страница");
         return "index";
     }
 
     @GetMapping("/uslugi")
     public String uslugi(Model model) {
+        model.addAttribute("title", "Страница с услугами");
         return "uslugi";
     }
     @GetMapping("/contacts")
     public String contacts(Model model) {
+        model.addAttribute("title", "Контакты");
         return "contacts";
     }
     @GetMapping("/about")
     public String about(Model model) {
+        model.addAttribute("title", "Про нас");
         return "about";
     }
 }
